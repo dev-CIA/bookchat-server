43a13cfc1ca1341f0116c277ff5ab001514cef87
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-01 22:31:54 +0900

    feat: openai를 사용한 채팅 서버 구현

--- index.js
+++ index.js
@@ -15,28 +15,36 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
+const messages = [
+  {
+    role: 'system',
+    content:
+      '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
+  },
+  {
+    role: 'user',
+    content:
+      '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
+  },
+  {
+    role: 'assistant',
+    content: '안녕하세요. 저는 책에 관한 토론이 즐거운 북챗입니다. 어떤 책에 대해 얘기해볼까요?',
+  },
+];
+
 app.post('/chat', async (req, res) => {
+  const { content, sender } = req.body;
+
+  if (content === '') return;
+  messages.push({ role: sender, content: content });
+
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
-    messages: [
-      {
-        role: 'system',
-        content:
-          '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
-      },
-      {
-        role: 'user',
-        content:
-          '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
-      },
-      {
-        role: 'assistant',
-        content: '안녕하세요. 저는 책에 관한 토론이 즐거운 북챗입니다. 어떤 책에 대해 얘기해볼까요?',
-      },
-    ],
+    messages: messages,
   });
   let bookchat = completion.data.choices[0].message['content'];
-  console.log(bookchat);
+  messages.push({ role: 'assistant', content: bookchat });
+
   res.send(bookchat);
 });
 
