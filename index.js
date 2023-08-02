e10db91c12f479461835f5316a7bf6273e998ae2
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-02 15:14:51 +0900

    refactor: openai api 로직 분리

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const { Configuration, OpenAIApi } = require('openai');
 
 require('dotenv').config();
 
@@ -9,44 +8,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-const messages = [
-  {
-    role: 'system',
-    content:
-      '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
-  },
-  {
-    role: 'user',
-    content:
-      '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
-  },
-  {
-    role: 'assistant',
-    content: '안녕하세요. 저는 책에 관한 토론이 즐거운 북챗입니다. 어떤 책에 대해 얘기해볼까요?',
-  },
-];
-
-app.post('/chat', async (req, res) => {
-  const { content, sender } = req.body;
-
-  if (content === '') return;
-  messages.push({ role: sender, content: content });
-
-  const completion = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    messages: messages,
-  });
-  let bookchat = completion.data.choices[0].message['content'];
-  messages.push({ role: 'assistant', content: bookchat });
-
-  res.send(bookchat);
-});
+app.use('/api/ai', require('./router/api/openai'));
 
 app.listen(PORT, () => {
   console.log(`🍭 Server listening at http://localhost:${PORT}`);
---
+++ router/api/openai.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+const messages = [
+  {
+    role: 'system',
+    content:
+      '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
+  },
+  {
+    role: 'user',
+    content:
+      '당신은 책 전문가이다. 모든 책을 다 읽었고, 모든 책의 정보를 명확하게 잘 알고 있다. 모든 책에 대해서 토론할 수 있다. 모든 책에 대한 질문에 명확하게 답할 수 있으며, 책에 대한 감상을 얘기할 수 있다.',
+  },
+  {
+    role: 'assistant',
+    content: '안녕하세요. 저는 책에 관한 토론이 즐거운 북챗입니다. 어떤 책에 대해 얘기해볼까요?',
+  },
+];
+
+router.post('/chat', async (req, res) => {
+  const { content, sender } = req.body;
+
+  if (content === '') return;
+  messages.push({ role: sender, content: content });
+
+  const completion = await openai.createChatCompletion({
+    model: 'gpt-3.5-turbo',
+    messages: messages,
+  });
+  let bookchat = completion.data.choices[0].message['content'];
+  messages.push({ role: 'assistant', content: bookchat });
+  console.log(messages);
+  res.send(bookchat);
+});
+
+module.exports = router;
