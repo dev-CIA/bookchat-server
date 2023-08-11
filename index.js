e3eccae9159e60993e12f10eee16359172c3a1cd
Inae Choi <112180368+dev-CIA@users.noreply.github.com>
2023-08-11 20:15:39 +0900

    rafactor: cors origin에서 로컬 서버 제거
    - 프론트 개발 환경에서 proxy 서버 설정함으로 인함

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 const corsOptions = {
-  origin: ['http://localhost:5173', /\.mybookchat\.com$/],
+  origin: [/\.mybookchat\.com$/],
   credentials: true,
 };
 
